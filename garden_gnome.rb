fdf478563d845aaaad876dab6eac4cea0dc0c83e
serendipidoussophia <scurranmoore@gmail.com>
2018-07-30 14:11:47 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,34 @@
 # Code your instances here
+class GardenGnome
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def age=(age)
+    @age = age
+  end
+  def age
+    @age
+  end
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end
+  def gluten_allergy
+    @gluten_allergy
+  end
+  def initialize(hat_color = "red")
+    @personality = "evil"
+    @hat_color = GardenGnome.new
+  end
+  def personality
+    @personality
+  end
+  def hat_color=(hat_color)
+    @hat_color = hat_color
+  end
+  def hat_color
+    @hat_color
+  end
+end
